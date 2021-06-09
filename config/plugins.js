385b9d1363279e8a176b7475584159f08b5ac080
armando zabala <armandozabala@gmail.com>
2021-06-09 16:46:35 -0500

    subiendo strapi cambios

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
+      cloud_name: env('CLOUDINARY_NAME','www-armandozabala-com'),
+      api_key: env('CLOUDINARY_KEY','472993854717965'),
+      api_secret: env('CLOUDINARY_SECRET','cMIkmdenzUScHh64BAtsSjzcrMg'),
     },
   },
   // ...
